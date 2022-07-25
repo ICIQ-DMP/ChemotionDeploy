be65a8c6ca985b37d1c6accbf45b1187a70d71b0
AleixMT <aleixaretra@gmail.com>
2022-07-25 18:27:41 +0200

    UPDATED: deploy chemotion working

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,8 @@ ensure_dependency()
       ${PACKAGE_MANAGER_INSTALL} "$1"
     else 
       # We are not root and dependency is not available, install using root
-      echo "Introduce password to install $1"
+      echo
+      echo "Introduce password to install $1 if asked"
       sudo ${PACKAGE_MANAGER_INSTALL} "$1"
     fi 
   fi 
@@ -41,21 +42,33 @@ main()
   done
 
   # Download the docker-compose file 
+  echo
   echo "Downloading docker compose file"
-  wget --show-progress https://raw.githubusercontent.com/ptrxyz/chemotion/latest-release/docker-compose.yml 
+  rm -f "${PROJECT_FOLDER}/docker-compose.yml*"
+  wget -q --show-progress https://raw.githubusercontent.com/ptrxyz/chemotion/latest-release/docker-compose.yml -O "${PROJECT_FOLDER}/docker-compose.yml"
 
   # Run it using docker-compose, depending on our privileges
   if isRoot; then 
+    docker-compose create
     docker-compose -d up
   else 
-    echo "Introduce password to run chemotion"
+    echo
+    echo "Introduce password to create container if asked"
+    sudo docker-compose create
+    echo "Introduce password to deploy container if asked"
     sudo docker-compose -d up
   fi 
 
   # Shows it in the browser, after active waiting
-  echo "Chemotion will be shown in the browser after the instance has been loaded."
-  sleep 10 
-  xdg-open "http://localhost:4000/"
+  echo
+  echo "Chemotion will be opened in the browser"
+
+  # Use xdg if available, if not use firefox, which is usually present in Linux systems
+  if command -v "xdg-open" &>/dev/null; then
+    xdg-open "http://localhost:4000/"
+  else 
+    firefox "http://localhost:4000/"
+  fi 
 }
 
 
@@ -67,3 +80,5 @@ PACKAGE_MANAGER_INSTALL="apt-get install -y"
 cd "${PROJECT_FOLDER}"
 main "$@"
 cd "${CURRENT_DIR}"
+
+exit 0
