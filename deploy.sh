22f9e0f9ace3ead6b61b5460f1e2454c69b5fcaf
AleixMT <aleixaretra@gmail.com>
2022-07-25 18:03:45 +0200

    UPDATED: deploy chemotion base script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,66 @@
 #!/usr/bin/env bash
 
-main()
+
+# Description: Ensures that a dependency (package) is present
+# Arguments:
+# - Argument 1: Dependency to ensure.
+ensure_dependency()
 {
-  wget https://raw.githubusercontent.com/ptrxyz/chemotion/latest-release/docker-compose.yml
+  # If $1 dependency is not present 
+  if ! command -v "$1"; then
+    if isRoot; then 
+      ${PACKAGE_MANAGER_INSTALL} "$1"
+    else 
+      # We are not root and dependency is not available, install using root
+      sudo ${PACKAGE_MANAGER_INSTALL} "$1"
+    fi 
+  fi 
 }
 
-CURRENT_DIR="$(pwd)
-export PROJECT_FOLDER="$(cd "$(dirname "$(realpath "$0")")" &>/dev/null && pwd)"
+
+# Description: Returns true (0) if the user executing the code is root and false (1) otherwise. 
+# Arguments:
+# - Argument 1: Dependency to ensure.
+isRoot()
+{
+  if [ "${EUID}" == 0 ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+
+# Description: Ensure all depenendcies for Chemotion software, deploys the Chemotion instance, runs it and shows it using the browser.
+main()
+{
+  dependencies=("wget" "docker" "docker-compose")
+  for dependency in ${dependencies[@]}; do 
+    ensure_dependency "${dependency}"
+  done
+
+  # Download the docker-compose file 
+  wget https://raw.githubusercontent.com/ptrxyz/chemotion/latest-release/docker-compose.yml 
+
+  # Run it using docker-compose, depending on our privileges
+  if isRoot; then 
+    docker-compose -d up
+  else 
+    sudo docker-compose -d up
+  fi 
+
+  # Shows it in the browser, after active waiting
+  echo "Chemotion will be shown in the browser after the instance has been loaded."
+  sleep 10 
+  xdg-open "http://localhost:4000/"
+}
+
+
+
+CURRENT_DIR="$(pwd)"
+PROJECT_FOLDER="$(cd "$(dirname "$(realpath "$0")")" &>/dev/null && pwd)"
+PACKAGE_MANAGER_INSTALL="apt-get install -y"
+
 cd "${PROJECT_FOLDER}"
 main "$@"
 cd "${CURRENT_DIR}"
